bb1752ef690a7229a1b9a8481e9022eeb0d1a972
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2022-08-11 17:55:23 -0300

    Criar componente sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { Header } from "./components/Header"
 
+import styles from './App.module.css';
+import { SideBar } from "./components/SideBar";
+
 function App() {
 
   return (
    <div>
     <Header />
-    <p>Hello word</p>
+    
+    <div className={styles.wrapper}>
+
+      <SideBar />
+
+      <main>
+        <p>
+          eduardo soares
+        </p>
+      </main>
+    </div>
    </div>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import styles from './header.module.css';
 
+import IgniteLogo from '../../assets/ignite-logo.svg';
+
 export function Header() {
   return (
     <header className={styles.header}>
-      Ignite Feed
+      <img src={IgniteLogo} alt="Logotipo do Ignite" />
     </header>
   )
 }
