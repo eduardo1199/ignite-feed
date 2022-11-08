a13fa2f276793fec741d218cc545710037265fa7
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2022-11-08 17:22:30 -0300

    adicionar extensão de propriedades img para componente de avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,15 +1,17 @@
+import { ImgHTMLAttributes } from 'react';
 import  styles from './avatar.module.css';
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   isAvatarComment?: boolean;
   avatarImg: string;
 }
 
-export function Avatar({ isAvatarComment, avatarImg }: AvatarProps) {
+export function Avatar({ isAvatarComment, avatarImg, ...props }: AvatarProps) {
   return  (
     <img 
       src={avatarImg}
       className={isAvatarComment ? styles.avatarDefault : styles.avatarBorder} 
+      {...props}
     />
   )
 }
